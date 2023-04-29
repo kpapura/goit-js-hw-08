6a107245643129cb7d3896f7bc59e8242e180496
kpapura <125825500+kpapura@users.noreply.github.com>
2023-04-29 13:29:29 +0300

    changes

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const galleryList = document.querySelector(".gallery");
+let markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
